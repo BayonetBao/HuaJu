a2286c07b3f588da3416948b0a6edd62ec3cd384
乔杨珂 <13526670136@163.com>
2018-06-05 21:35:57 +0800

    buildmapper 2018-6-5

--- src/main/java/com/huaju/dao/BuildMapper.java
+++ src/main/java/com/huaju/dao/BuildMapper.java
@@ -1,8 +1,12 @@
 package com.huaju.dao;
 
 import com.huaju.entity.Build;
+import com.huaju.entity.BuildQueryPojo;
+
+import java.util.List;
 
 public interface BuildMapper {
     //添加楼盘
     public void setBuild(Build build);
+    public List<Build> selectBuildQueryPojo(BuildQueryPojo buildQueryPojo);
 }
